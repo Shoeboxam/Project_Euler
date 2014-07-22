006a7743dd435e7c33f6525af05d6a3564e0b8e3
Shoeboxam <shoeboxam@gmail.com>
2014-07-21 22:20:19 -0500

    9_cleanup

--- Python/9_special_pythagorean_number.py
+++ Python/9_special_pythagorean_number.py
@@ -1,7 +1,6 @@
 import numpy as np
 from numpy import linalg
 
-zone = 1000
 
 def pythagorean_brute():
   for index_a in range(1, zone):
@@ -11,38 +10,46 @@ def pythagorean_brute():
           return (index_a, index_b, index_c)
   return False
 
-def price_linear_transformation(matrix_library):
+def pythagorean_primaries():
+  def price_linear_transformation(matrix_library):
 
-  #STORE pythagorean triples
-  triple_store = []
+    #STORE pythagorean triples
+    triple_store = []
 
-  #CALCULATE dot product of input triple with transposed transform-matrix
-  for triple in matrix_library:
-    print(triple)
-    triple_store.append(triple * lin_trans_A.T)
-    triple_store.append(triple * lin_trans_B.T)
-    triple_store.append(triple * lin_trans_C.T)
+    #CALCULATE dot product of input triple with transposed transform-matrix
+    for triple in matrix_library:
+      #print(triple)
+      triple_store.append(triple * lin_trans_A.T)
+      triple_store.append(triple * lin_trans_B.T)
+      triple_store.append(triple * lin_trans_C.T)
 
-  #CHECK for triple that meets criteria
-  for triple in triple_store:
-    if np.prod(triple) == zone:
-      solution = triple
+    #print("~~~~")
 
-  print("~~~~")
-
-  return triple_store
+    return triple_store
 
 
-#main()
-#STORE Price transformation matrices
-lin_trans_A = np.matrix([[2, 1, -1], [-2, 2, 2], [-2, 1, 3]])
-lin_trans_B = np.matrix([[2, 1, 1], [2, -2, 2], [2, -1, 3]])
-lin_trans_C = np.matrix([[2, -1, 1], [2, 2, 2], [2, 1, 3]])
+  #STORE Price transformation matrices
+  lin_trans_A = np.matrix([[2, 1, -1], [-2, 2, 2], [-2, 1, 3]])
+  lin_trans_B = np.matrix([[2, 1, 1], [2, -2, 2], [2, -1, 3]])
+  lin_trans_C = np.matrix([[2, -1, 1], [2, 2, 2], [2, 1, 3]])
 
-matrix_library = [np.array([3,4,5])]
-solution = np.array([0,0,0])
+  matrix_library = [np.array([3,4,5])]
+  solution = np.array([0,0,0])
 
-#LOOP until answer is found
-while (solution == np.array([0,0,0])).all():
-  #CALCULATE new set of triples
-  matrix_library = price_linear_transformation(matrix_library)
+  #LOOP until answer is found
+  while (solution == np.array([0,0,0])).all():
+    #CALCULATE new set of triples
+    matrix_library = price_linear_transformation(matrix_library)
+
+    #CHECK for triple that meets criteria
+    for triple in matrix_library:
+      if np.prod(triple) == zone:
+        return triple
+
+  return False
+
+
+#main() ~~~~~~~~~
+zone = 90167220
+print(pythagorean_primaries())
+#pythagorean_brute()
