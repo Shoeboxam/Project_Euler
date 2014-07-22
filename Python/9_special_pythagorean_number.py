0e3b779e8b527873d577e24e566541a225e32d75
Shoeboxam <shoeboxam@gmail.com>
2014-07-21 20:58:57 -0500

    9_Linear_Transformations

--- Python/9_special_pythagorean_number.py
+++ Python/9_special_pythagorean_number.py
@@ -1,7 +1,9 @@
 import numpy as np
 from numpy import linalg
 
-def pythagorean_brute(zone):
+zone = 1000
+
+def pythagorean_brute():
   for index_a in range(1, zone):
     for index_b in range(1, zone):
       for index_c in range(1, zone):
@@ -9,44 +11,38 @@ def pythagorean_brute(zone):
           return (index_a, index_b, index_c)
   return False
 
-def price_linear_transformation(matrix_library, depth):
-  for calculate in matrix_library[-1:]:
-    calculate = [base * linear_transformation[0].T, base * linear_transformation[1].T, base * linear_transformation[2].T]
-    matrix_library[depth].append(calculate)
-  print matrix_library
-  return matrix_library
+def price_linear_transformation(matrix_library):
+
+  #STORE pythagorean triples
+  triple_store = []
+
+  #CALCULATE dot product of input triple with transposed transform-matrix
+  for triple in matrix_library:
+    print(triple)
+    triple_store.append(triple * lin_trans_A.T)
+    triple_store.append(triple * lin_trans_B.T)
+    triple_store.append(triple * lin_trans_C.T)
+
+  #CHECK for triple that meets criteria
+  for triple in triple_store:
+    if np.prod(triple) == zone:
+      solution = triple
+
+  print("~~~~")
+
+  return triple_store
 
 
-  #if depth >= 1:
-  #  print price_linear_transformation(base * linear_trans_A.T, depth)
-  #  print price_linear_transformation(base * linear_trans_B.T, depth)
-  #  print price_linear_transformation(base * linear_trans_C.T, depth)
+#main()
+#STORE Price transformation matrices
+lin_trans_A = np.matrix([[2, 1, -1], [-2, 2, 2], [-2, 1, 3]])
+lin_trans_B = np.matrix([[2, 1, 1], [2, -2, 2], [2, -1, 3]])
+lin_trans_C = np.matrix([[2, -1, 1], [2, 2, 2], [2, 1, 3]])
 
-zone = 60
-ceiling = 8
-depth = 0
-base = np.matrix([5,12,13])
+matrix_library = [np.array([3,4,5])]
+solution = np.array([0,0,0])
 
-#used in price linear matrix function
-linear_transformation= [np.matrix('2 1 -1; -2 2 2; -2 1 3'),
-                        np.matrix('2 1 1; 2 -2 2; 2 -1 3'),
-                        np.matrix('2 -1 1; 2 2 2; 2 -1 1')]
-
-matrix_library = [0] * depth
-#unfortunately used an array of lists of matrices
-while depth <= ceiling:
-  matrix_library = price_linear_transformation(matrix_library, depth)
-  depth += 1
-else:
-  print matrix_library
-
-print price_linear_transformation(base, depth)
-
-total = 1
-indices = False
-if indices != False:
-  for value in indices:
-    total *= value
-  print total
-else:
-  pass #print "No Pythagorean triple found"
+#LOOP until answer is found
+while (solution == np.array([0,0,0])).all():
+  #CALCULATE new set of triples
+  matrix_library = price_linear_transformation(matrix_library)
